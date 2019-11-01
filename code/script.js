03b98ddee2387a598182e7fa991e1cb0c26f9291
MateuszWalowski <mat.walowski@gmail.com>
2019-11-01 12:39:47 +0100

    doneiguess

--- code/script.js
+++ code/script.js
@@ -1,21 +1,30 @@
-function myPlayFunction1() {
+//ANIMATION 1
+
+function play1() {
+    var element = document.getElementById("animation1");
+    element.classList.remove("animation1");
+    void element.offsetWidth;
+    element.classList.add("animation1");
     document.getElementById("animation1").style.animationPlayState = "running";
-    document.getElementById('effect1').play()
-}
-
-
-
-function myPauseFunction1() {
-    document.getElementById("animation1").style.animationPlayState = "paused";
-    document.getElementById('effect1').pause()
-}
-
-function changeColor1() {
+    var element = document.getElementById("effect1");
+    element.classList.remove("effect1");
+    void element.offsetWidth;
+    element.classList.add("effect1");
+    document.getElementById('effect1').currentTime = 0;
+    document.getElementById('effect1').play();
     document.getElementById("start1").style.backgroundColor = "#ff0000";
 }
 
-function changeColorpause1() {
+function pause1() {
+    document.getElementById("animation1").style.animationPlayState = "paused";
+    document.getElementById('effect1').pause();
     document.getElementById("start1").style.backgroundColor = "#e7e7e7";
+}
+
+function resume1() {
+    document.getElementById("animation1").style.animationPlayState = "running";
+    document.getElementById('effect1').play();
+    document.getElementById("start1").style.backgroundColor = "#ff0000"
 }
 
 
@@ -29,11 +38,43 @@ function restart1() {
     document.getElementById('effect1').pause();
     document.getElementById('effect1').currentTime = 0;
 }
+//ANIMATION 2
 
-//function playaudio() {
-//    document.getElementById('effect').play()
-//}
+function play2() {
+    var element = document.getElementById("animation2");
+    element.classList.remove("animation2");
+    void element.offsetWidth;
+    element.classList.add("animation2");
+    document.getElementById("animation2").style.animationPlayState = "running";
+    var element = document.getElementById("effect2");
+    element.classList.remove("effect2");
+    void element.offsetWidth;
+    element.classList.add("effect2");
+    document.getElementById('effect2').currentTime = 0;
+    document.getElementById('effect2').play();
+    document.getElementById("start2").style.backgroundColor = "#ff0000";
+}
 
-//function pauseaudio() {
-//    document.getElementById('effect').pause()()
-//}
+function pause2() {
+    document.getElementById("animation2").style.animationPlayState = "paused";
+    document.getElementById('effect2').pause();
+    document.getElementById("start2").style.backgroundColor = "#e7e7e7";
+}
+
+function resume2() {
+    document.getElementById("animation2").style.animationPlayState = "running";
+    document.getElementById('effect2').play();
+    document.getElementById("start2").style.backgroundColor = "#ff0000"
+}
+
+
+function restart2() {
+    var element = document.getElementById("animation2");
+    element.classList.remove("animation2");
+    void element.offsetWidth;
+    element.classList.add("animation2");
+    document.getElementById("start2").style.backgroundColor = "#e7e7e7";
+    document.getElementById("animation2").style.animationPlayState = "paused";
+    document.getElementById('effect2').pause();
+    document.getElementById('effect2').currentTime = 0;
+}
