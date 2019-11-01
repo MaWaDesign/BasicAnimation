79c64a605f11044b35917f2f1ce0d5da3663033d
MateuszWalowski <mat.walowski@gmail.com>
2019-11-01 08:07:34 +0100

    buttons

--- code/script.js
+++ code/script.js
@@ -1,34 +1,15 @@
-var date = new Date()
-
-document.body.innerHTML = "<h1> The date today is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>";
-
-
-function animation1() {
-    var elem = document.getElementById("animate");
-    var pos = 0;
-    var id = setInterval(frame, 1);
-
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.left = pos + "px";
-        }
-    }
+function myPlayFunction() {
+    document.getElementById("myDIV").style.animationPlayState = "running";
 }
 
-function animation2() {
-    var elem = document.getElementById("animate2");
-    var pos = 0;
-    var id = setInterval(frame, 1);
+function myPauseFunction() {
+    document.getElementById("myDIV").style.animationPlayState = "paused";
+}
 
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.left = pos + "px";
-        }
-    }
+function changeColor() {
+    document.getElementById("start").style.backgroundColor = "#ff0000";
+}
+
+function changeColorpause() {
+    document.getElementById("start").style.backgroundColor = "#e7e7e7";
 }
