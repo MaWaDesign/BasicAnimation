45715f6162a6f11f0241e7dbb07e48f8705a41d3
MateuszWalowski <mat.walowski@gmail.com>
2019-11-01 10:49:32 +0100

    buttons final + music

--- code/script.js
+++ code/script.js
@@ -1,15 +1,39 @@
-function myPlayFunction() {
-    document.getElementById("myDIV").style.animationPlayState = "running";
+function myPlayFunction1() {
+    document.getElementById("animation1").style.animationPlayState = "running";
+    document.getElementById('effect1').play()
 }
 
-function myPauseFunction() {
-    document.getElementById("myDIV").style.animationPlayState = "paused";
+
+
+function myPauseFunction1() {
+    document.getElementById("animation1").style.animationPlayState = "paused";
+    document.getElementById('effect1').pause()
 }
 
-function changeColor() {
-    document.getElementById("start").style.backgroundColor = "#ff0000";
+function changeColor1() {
+    document.getElementById("start1").style.backgroundColor = "#ff0000";
 }
 
-function changeColorpause() {
-    document.getElementById("start").style.backgroundColor = "#e7e7e7";
+function changeColorpause1() {
+    document.getElementById("start1").style.backgroundColor = "#e7e7e7";
 }
+
+
+function restart1() {
+    var element = document.getElementById("animation1");
+    element.classList.remove("animation1");
+    void element.offsetWidth;
+    element.classList.add("animation1");
+    document.getElementById("start1").style.backgroundColor = "#e7e7e7";
+    document.getElementById("animation1").style.animationPlayState = "paused";
+    document.getElementById('effect1').pause();
+    document.getElementById('effect1').currentTime = 0;
+}
+
+//function playaudio() {
+//    document.getElementById('effect').play()
+//}
+
+//function pauseaudio() {
+//    document.getElementById('effect').pause()()
+//}
